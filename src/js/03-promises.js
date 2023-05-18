829892fc4e88acc0871146635df7938de91f6eff
Dmytro Kozlovskyi <120205405+Dmytroukraine@users.noreply.github.com>
2023-05-18 14:18:27 +0300

    task 3 fix

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,4 +1,4 @@
-    import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 Notiflix.Notify.init({ position: 'center-top' });
 
@@ -11,22 +11,20 @@ function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       const shouldResolve = Math.random() > 0.3;
-      const result = {position, delay}
-
+      const result = { position, delay };
 
       if (shouldResolve) {
         // Fulfill
-        resolve(result) 
-       } else {
+        resolve(result);
+      } else {
         // Reject
-        reject(result)
-        }
+        reject(result);
+      }
     }, delay);
   });
-      
 }
 
-formEl.addEventListener('submit', handleSubmit)
+formEl.addEventListener('submit', handleSubmit);
 
 function handleSubmit(event) {
   event.preventDefault();
@@ -38,17 +36,15 @@ function handleSubmit(event) {
   const amountValue = Number(amountEl.value);
   formEl.reset();
 
-  
-
   for (let i = 1; i <= amountValue; i += 1) {
-          createPromise(i, delayValue)  
-          .then(({ position, delay }) => {
-      Notiflix.Notify.success(`Fulfilled promise ${position} in ${delay}ms`); 
-    })
+    createPromise(i, delayValue)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
+      })
 
-    .catch(({ position, delay }) => {
-          Notiflix.Notify.failure(`Rejected promise ${position} in ${delay}ms`); 
-    });
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(`Rejected promise ${position} in ${delay}ms`);
+      });
 
     delayValue += stepValue;
   }
